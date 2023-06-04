695039c74e22f1a182d190fbba9a3a894ec564d1
Altarois <37486217+Altarois@users.noreply.github.com>
2023-06-04 22:29:14 +0200

    Update test_phone_calculation.py

--- test_phone_calculation.py
+++ test_phone_calculation.py
@@ -7,7 +7,7 @@ from phone_calculation import compute_minute_types
 
 class TestPhoneCalculation(unittest.TestCase):
     def test_compute_minute_types(self):
-        # Test case 1: Call starts and ends during the day
+        # Test case 1: Call starts and ends during the normal tarification
         start = datetime(2023, 6, 1, 6, 15)
         end = datetime(2023, 6, 1, 6, 30)
         night_minutes, day_minutes, bonus_minutes = compute_minute_types(start, end)
@@ -15,7 +15,7 @@ class TestPhoneCalculation(unittest.TestCase):
         self.assertEqual(day_minutes, 0)
         self.assertEqual(bonus_minutes, 9)
 
-        # Test case 2: Call starts during the night and ends during the day
+        # Test case 2: Call starts during the reduced tarification and ends during the normal tarification
         start = datetime(2023, 6, 1, 7, 57)
         end = datetime(2023, 6, 1, 8, 27)
         night_minutes, day_minutes, bonus_minutes = compute_minute_types(start, end)
@@ -23,7 +23,7 @@ class TestPhoneCalculation(unittest.TestCase):
         self.assertEqual(day_minutes, 2)
         self.assertEqual(bonus_minutes, 24)
 
-        # Test case 3: Call starts and ends during the night
+        # Test case 2: Call starts during the normal tarification and ends during the reduced tarification
         start = datetime(2023, 6, 1, 15, 58)
         end = datetime(2023, 6, 1, 16, 2)
         night_minutes, day_minutes, bonus_minutes = compute_minute_types(start, end)
